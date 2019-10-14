aaa9378a6f794d7924f02e06c5ed357c20a6f3b3
alephshahor <adrianalvarezleon@gmail.com>
2019-10-14 20:41:16 +0100

    Deleted the ImageViewer widget class and added clone() to MainWindow
    Now the same functionality happens in MainWindow and new windows can be opened from there.
    One way in which images can be opened is by using the clone function.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,19 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include "include/image.h"
-#include "imageviewer.h"
 
 #include <iostream>
 #include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
+      mImagePixMap(),
+      mImage(),
       ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     connectSignals();
+    scaleImage();
 }
 
 MainWindow::~MainWindow()
@@ -19,16 +21,41 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+Image MainWindow::image() const
+{
+    return mImage;
+}
+
+void MainWindow::setImage(const Image &image)
+{
+    mImage = image;
+    mImagePixMap.convertFromImage(mImage.image());
+    ui -> imageLabel -> setPixmap(mImagePixMap);
+}
+
+void MainWindow::clone()
+{
+    MainWindow* newWindow = new MainWindow();
+    newWindow -> setImage(mImage);
+    newWindow -> show();
+}
+
 void MainWindow::connectSignals()
 {
     connect(ui -> actionOpen, &QAction::triggered,
             this, &MainWindow::openImageViewer);
+    connect(ui -> actionClone, &QAction::triggered,
+            this, &MainWindow::clone);
 }
 
 void MainWindow::openImageViewer(){
     QString filepath = QFileDialog::getOpenFileName((this), "Open the file");
     Image image = Image(filepath);
-    ImageViewer* imageviewer = new ImageViewer();
-    imageviewer -> setImage(image);
-    imageviewer -> show();
+    setImage(image);
+}
+
+void MainWindow::scaleImage()
+{
+    ui -> imageLabel -> setScaledContents( true );
+    ui -> imageLabel -> setSizePolicy( QSizePolicy::Ignored, QSizePolicy::Ignored );
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
+#include "include/image.h"
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -15,6 +15,24 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    /*!
+     * \brief  mImage's getter.
+     * \return mImage.
+     */
+    Image image() const;
+
+    /*!
+     * \brief sets mImage and mImagePixmap attributes.
+     * \param image
+     */
+    void setImage(const Image &image);
+
+    /*!
+     * \brief clone Opens a new window with the current
+     * image.
+     */
+    void clone();
+
 private:
     /*!
      * \brief connectSignals connects menu options
@@ -27,6 +45,21 @@ private:
      * widget in a new window.
      */
     void openImageViewer();
+
+    /*!
+     * \brief scales and sets the size policy of the
+     * ui's QLabel's element.
+     */
+    void scaleImage();
+
+private:
+
+    /*!
+     * \brief mImagePixMap is a QPixmap needed to
+     * represent the image in the GUI.
+     */
+    QPixmap mImagePixMap;
+    Image mImage;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
