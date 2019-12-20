425ee62f8f63c0c9f4d7a8050b02a41cb1dd2b2c
alephshahor <adrianalvarezleon@gmail.com>
2019-12-20 21:18:54 +0000

    Direct transform working

--- rotation.cpp
+++ rotation.cpp
@@ -37,7 +37,7 @@ void Rotation::rotateImage()
     }
 }
 
-void Rotation::directTransform(float rotationAngle)
+void Rotation::inverseTransform(float rotationAngle)
 {
     std::vector<std::vector<float>> rotationMatrix = generateRotationMatrix(rotationAngle);
 
@@ -91,9 +91,63 @@ void Rotation::directTransform(float rotationAngle)
     emit imageChanged(rotatedImage);
 }
 
-void Rotation::inverseTransform(float rotationAngle)
-{
 
+void Rotation::directTransform(float rotationAngle)
+{
+    std::vector<std::vector<float>> rotationMatrix = generateRotationMatrix(rotationAngle);
+
+    int width = this -> image.getWidth();
+    int height = this -> image.getHeight();
+
+    int leftMostUpperIndex[2] =  {0,0};
+    int leftMostLowerIndex[2] =  {int(height * (-1) * rotationMatrix[0][1]), int(height  * rotationMatrix[1][1])};
+    int rightMostUpperIndex[2] = {int(width * rotationMatrix[0][0]), int(width * rotationMatrix[1][0])};
+    int rightMostLowerIndex[2] = {int(width * rotationMatrix[0][0]) - int(height * rotationMatrix[0][1]),
+                                  int(width * rotationMatrix[1][0]) + int(height * rotationMatrix[1][1])};
+
+    qDebug() << "LMU" << leftMostUpperIndex[0] << leftMostUpperIndex[1];
+    qDebug() << "LML" << leftMostLowerIndex[0] << leftMostLowerIndex[1];
+    qDebug() << "RMU" << rightMostUpperIndex[0] << rightMostUpperIndex[1];
+    qDebug() << "RML" << rightMostLowerIndex[0] << rightMostLowerIndex[1];
+
+    std::vector<int> indexes = {leftMostUpperIndex[0], leftMostLowerIndex[0], rightMostLowerIndex[0], rightMostUpperIndex[0]};
+    int maxWidth = maxIndex(indexes);
+    int minWidth = minIndex(indexes);
+
+    indexes = {leftMostUpperIndex[1], leftMostLowerIndex[1], rightMostLowerIndex[1], rightMostUpperIndex[1]};
+    int maxHeight = maxIndex(indexes);
+    int minHeight = minIndex(indexes);
+
+    int newWidth = abs(maxWidth - minWidth);
+    int newHeight = abs(maxHeight - minHeight);
+
+    qDebug() << "Cos theta" << rotationMatrix[0][0];
+    qDebug() << "Sin theta" << rotationMatrix[0][1];
+    qDebug() << "Sin theta" << rotationMatrix[1][0];
+    qDebug() << "Cos theta" << rotationMatrix[1][1];
+    qDebug() << "Min width: " << minWidth;
+    qDebug() << "Max width: " << maxWidth;
+    qDebug() << "Min height: " << minHeight;
+    qDebug() << "Max height: " << maxHeight;
+
+    Image rotatedImage(this -> image,newWidth,newHeight);
+    for(int i = 0; i < this -> image.getHeight(); i++){
+        for(int j = 0; j < this -> image.getWidth(); j++){
+
+            int rotatedXCoordinate = (j * rotationMatrix[0][0]) - (i * rotationMatrix[0][1]);
+            int rotatedYCoordinate = (j * rotationMatrix[0][1]) + (i * rotationMatrix[1][1]);
+
+            rotatedXCoordinate -= minWidth;
+            rotatedYCoordinate -= minHeight;
+
+            QRgb colorValue = this -> image.getImage().pixel(j,i);
+
+            rotatedImage.getImage().setPixel(rotatedXCoordinate, rotatedYCoordinate,colorValue);
+
+        }
+    }
+
+    emit imageChanged(rotatedImage);
 }
 std::vector<std::vector<float> > Rotation::generateRotationMatrix(float rotationAngle)
 {
